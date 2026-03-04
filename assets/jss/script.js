d1e6510f12ced6a58a0be033f44b697ac1033963
priyankasingh125 <priyankasingh125@gmail.com>
2026-03-04 02:06:13 +0000

    committed

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -14,7 +14,8 @@ let ans=[];
 
 // Event listener for DOMContentLoaded
 document.addEventListener("DOMContentLoaded", function () 
-{   window.alert("Welcome to the Quiz!");
+{   
+    window.alert("Welcome to the Quiz!");
 	document.getElementById('quiz').style.display = "none"
 	document.getElementById('next-btn').style.visibility = true;
 	startButton.addEventListener('click', StartQuiz);
@@ -49,13 +50,13 @@ document.addEventListener("DOMContentLoaded", function ()
 }); 
   
     function ResetQuestion()
-	{  
+   {  
 	//Clear The Answer Grid
     while (answerbuttons.firstChild)
-    {
+   {
     answerbuttons.removeChild(answerbuttons.firstChild);
-	}
-	} 
+   }
+  } 
     let snapshotQuestion;	
     let currentquestion;
     let questions = [{
@@ -100,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function ()
         ],
 		selectval: false
     },
-	    {
+	{
         question: 'Which SQL Statement is used to Delete Data from database?',
         answers: [{
                 text: 'DELETE',
@@ -163,7 +164,7 @@ document.addEventListener("DOMContentLoaded", function ()
         ],
 		selectval: false
     },
-	{
+{
         question: 'How do you select all columns from a table name Persons?',
         answers: [{
                 text: 'SELECT * FROM Persons',
@@ -336,7 +337,10 @@ function showQuestion()
 	});
 	document.querySelectorAll('input[name="answer"]')[ans[CurrentQuestionIndex]].checked=true;
     selected=  document.querySelectorAll('input[name="answer"]')[ans[CurrentQuestionIndex]].checked ;
-	}
+
+	
+
+}
  	
 function selectAnswer(radio,answer)
 { 
@@ -361,52 +365,31 @@ function selectAnswer(radio,answer)
 	}
 	Array.from(answerbuttons.children).forEach((btn)=>{
 	radio.classList.add("ansbtn");
-	btn.disabled=true;
+	btn.se
 	if(btn.innerText===questions[CurrentQuestionIndex].answers.find(a=>a.correct).text)
     {
 	btn.style.backgroundColor= "#003A8F";
+	
+	}
+	else
+	{
+		btn.style.backgroundColor= "#FF0000";
+		
+		
 	}
 	});
+	const radios = document.querySelectorAll('input[name="answer"]');
+	radios.forEach(r => r.disabled = true);
  } 
 	 
-   function previousQuestion()
-{
-	ResetQuestion();
-	//Get The Question from the Array And Append to the answer grid
-	let currentquestion = questions[CurrentQuestionIndex];
-	questionElt.innerHTML =currentquestion.question;
-	currentquestion.answers.forEach((answer,index)=> {
-    const label = document.createElement("label");
-	const radio = document.createElement("input");
-	radio.type = "radio";
-	radio.name = "answer";
-	radio.value = index;
-	const span = document.createElement("span");
-	span.textContent= answer.text;
-	label.appendChild(radio);
-	radio.checked =currentquestion.selectval;
-	label.appendChild(span);
-	answerbuttons.appendChild(label);
-	const breakDiv = document.createElement('div');
-    breakDiv.className = 'flex-break';
-    answerbuttons.appendChild(breakDiv);
-	document.querySelectorAll('input[name="answer"]').forEach(r=>r.checked = false);
-    if(answers[CurrentQuestionIndex] !== undefined)
-	{
-		document.querySelectorAll('input[name="answer"]')[answers[CurrentQuestionIndex]].checked=true;
-	}
-	});	
-}
-
-      
-	 
-  function ShowScore()
-   {  
+ function ShowScore()
+{  
     ResetQuestion();
 	let TEXT = "YOU SCORED   " + figure;
    	document.getElementById('question').innerHTML = TEXT  +  " OUT OF "  +  CurrentQuestionIndex +" CLICK "+"START "+" AGAIN !!" ;
 	//document.getElementById('answer-buttons').innerText = " CLICK "+"START TO"+" PLAY AGAIN " ;
     document.getElementById('next-btn').style.display = "none";
+	document.getElementById('back-btn').style.display = "none";
 	document.getElementById('radiocontainer').style.display = "none";
 	//document.getElementById('question').innerHTML = "  ";
     //("You Scored " + score + "in the Quiz Today out of 10");
